ce4abf49bc2dcdb0d716e8dbe2ba9cc7d5c34730
Franek Suszko <franeksu@gmail.com>
2024-12-20 23:15:11 +0100

    fix: pydantic validation errors

--- backend/config.py
+++ backend/config.py
@@ -3,9 +3,9 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    webhook_secret: str
-    jenkins_user: str
-    jenkins_api_token: str
+    webhook_secret: str = 'mocksecret123'
+    jenkins_user: str = 'mockuser123'
+    jenkins_api_token: str = 'mockapitoken123'
     base_dir: str = os.path.abspath(
         os.path.join(os.path.dirname(os.path.abspath(__file__)), "webfs")
     )
