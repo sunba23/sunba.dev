57c2ddbfb85d456f7cee275a324824efe5aed367
Franek Suszko <franeksu@gmail.com>
2024-12-20 23:35:47 +0100

    fix: vite envvar and font

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,12 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   build: {
     rollupOptions: {
       external: [],
     },
   },
-};
-
+  define: {
+    'import.meta.env.VITE_WS_URL': JSON.stringify(process.env.VITE_WS_URL),
+  },
+});
